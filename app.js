0e1de8508ce065ec64032f5130b03bcffaa135b6
yomowataru <132421740+yomowataru@users.noreply.github.com>
2023-10-15 19:46:23 +0900

    5章　jQueryでHTMLのCSS要素を操作

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-$(document).ready(function (){
-  $('body').html('<h1>Hello jQuery!!<h1>');
+// Hello jQuery!! と表示------------------------------
+// $(document).ready(function (){
+//   $('body').html('<h1>Hello jQuery!!<h1>');
+// });
+
+// CSSのスタイルをjQueryで変更------------------------------
+// $(function () {
+//   $('.box1').css({
+//     'background-color': '#0000ff',
+//     'height': '200px'
+//   });
+// });
+
+// 上から下へスライドするアニメーション------------------------------
+// $(function(){
+//   $('.box1').slideDown('2000');
+// });
+
+// $(function(){
+//   $('.box1').slideUp('slow');
+// });
+
+// 表示、非表示------------------------------
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
+// 要素を上から下へスライドさせた後、赤色の正方形を青色の長方形（幅200px、高さ100px）に変更してください。
+// その後、下から上へスライドされるようにしてください。
+$(function (){
+  $('.box1').slideDown(2000,function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(3000);
+  });
 });
