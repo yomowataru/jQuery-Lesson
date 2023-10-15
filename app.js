59f284a2a0b0331edaffa47aca871b5265b5c03d
yomowataru <132421740+yomowataru@users.noreply.github.com>
2023-10-15 20:25:46 +0900

    6章　イベント検出

--- app.js
+++ app.js
@@ -32,12 +32,43 @@
 
 // 要素を上から下へスライドさせた後、赤色の正方形を青色の長方形（幅200px、高さ100px）に変更してください。
 // その後、下から上へスライドされるようにしてください。
-$(function (){
-  $('.box1').slideDown(2000,function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(3000);
+// $(function (){
+//   $('.box1').slideDown(2000,function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp(3000);
+//   });
+// });
+
+// マウスオーバー、マウスアウトイベント------------------------------
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// class属性を定義し、簡潔に------------------------------
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');    // .box1-extと書くと、(.)が.addClassと重複して【..box1-ext】という意味になるため注意
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// マウスクリックイベント------------------------------
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  })
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
+
